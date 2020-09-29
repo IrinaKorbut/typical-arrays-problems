8af1e1b9cfc4270fc43fcbabe85075f3185ed3c8
IrinaKorbut <irisharomka96@gmail.com>
2020-09-29 21:01:36 +0300

    feat: implement max, min, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+let result = 0;
+exports.min = function min(array) {
+    if (!array || !array.length) return 0;
+    result = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (result > array[i]) result = array[i];
+    }
+    return result;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || !array.length) return 0;
 
-exports.max = function max (array) {
-  return 0;
-}
+    result = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (result < array[i]) result = array[i];
+    }
+    return result;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || !array.length) return 0;
+    let sumArray = 0;
+    array.forEach((element) => {
+        sumArray += element;
+    });
+    result = sumArray / array.length;
+    return result;
+};
